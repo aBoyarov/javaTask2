bbea8a0e303eb0b86959ec6debd78bdb5953319b
Андрей <boyarov1995@yandex.ru>
2021-12-23 15:57:03 +0300

    character

---
+++ src/com/company/Character.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Character {
+    int gold;
+    int silver;
+    double wood;
+    byte health;
+    byte helpersNumber;
+
+    public Character(
+            int characterGold,
+            int characterSilver,
+            double characterWood,
+            byte characterHealth,
+            byte characterHelpersNumber
+    ) {
+        gold = characterGold;
+        silver = characterSilver;
+        wood = characterWood;
+        health = characterHealth;
+        helpersNumber = characterHelpersNumber;
+    }
+}
+
